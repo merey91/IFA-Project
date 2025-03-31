6e945426cde52ffa104e5d80f936f80c6c425690
Miro-DevOps <66559331+merey91@users.noreply.github.com>
2025-03-31 22:50:27 +1100

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,24 +18,24 @@ encrypt        = true                   # Enable state encryption
 resource "aws_instance" "jenkins-server" {  
 ami                    = "ami-" # replace / Amazon Linux 2023 AMI
 instance_type          = "t2.micro"
-key_name               = var.key_name
+key_name               = var.key_name #?
 vpc_security_group_ids = [aws_security_group.jenkins_sg.id]
 
-user_data = file("${path.module}/user-data.sh") # a bootstrapping script that automates the installation of software and system configuration upon instance launch.
+user_data = file("${path.module}/user-data.sh") # ? a bootstrapping script that automates the installation of software and system configuration upon instance launch.
 
 tags = {
 Name  = "Jenkins-Server" # aws ec2  instance name
-Usage = "pipeline"
+Usage = "pipeline" #?
 }
 }
 
-# Allocate an Elastic IP. Elastic IPs are static IP addresses designed for dynamic cloud computing, providing a fixed IP address for your instance.
+# Allocate an Elastic IP. Elastic IPs are static IP addresses designed for dynamic cloud computing, providing a fixed IP address for your instance.?
 
 resource "aws_eip" "jenkins_eip" {
 domain   = "vpc"
 }
 
-# Associate the Elastic IP with the Jenkins EC2 instance
+# Associate the Elastic IP with the Jenkins EC2 instance?
 
 resource "aws_eip_association" "jenkins_eip_assoc" {
 instance_id   = aws_instance.jenkins.id
