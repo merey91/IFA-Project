2d1f72c0acbfb79046f439a1e4eab80e2b2bd8e5
Miro-DevOps <66559331+merey91@users.noreply.github.com>
2025-04-04 10:09:29 +1100

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -3,3 +3,9 @@ output "alb_dns_name" {
   description = "The DNS name of the load balancer"
   value       = module.alb.dns_name
 }
+
+#
+output "jenkins_elastic_ip" {
+  description = "Elastic IP of the Jenkins instance"
+  value       = aws_eip.jenkins_eip.public_ip
+}
