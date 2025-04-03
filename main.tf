f656df64b26c080ecfa91562c5fee149b061d37a
Miro-DevOps <66559331+merey91@users.noreply.github.com>
2025-04-04 10:32:20 +1100

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,12 +14,26 @@ encrypt        = true                   # Enable state encryption
 }
 }
 
+# Main resources
+resource "aws_instance" "jenkins-ifa" {
+  ami                    = "ami-0013d898808600c4a" # Amazon Linux 2023 AMI
+  instance_type          = "t3.medium"
+  key_name               = var.key_name
+  vpc_security_group_ids = [aws_security_group.jenkins_sg.id]
+
+  user_data = file("${path.module}/user-data.sh") # ? a bootstrapping script that automates the installation of software and system configuration upon instance launch.
+
+
+  tags = {
+    Name  = "Jenkins-Server"
+    Usage = "pipeline"
+  }
+}
+
 # Create an S3 bucket resource
 resource "aws_s3_bucket" "my_bucket" {
   bucket = "my-shuotestbucket"
   acl    = "private"
-
-user_data = file("${path.module}/user-data.sh") # ? a bootstrapping script that automates the installation of software and system configuration upon instance launch.
 
 tags = {
     Name        = "My bucket"
@@ -28,13 +42,11 @@ tags = {
 }
 
 # Allocate an Elastic IP. Elastic IPs are static IP addresses designed for dynamic cloud computing, providing a fixed IP address for your instance.?
-
 resource "aws_eip" "jenkins_eip" {
 domain   = "vpc"
 }
 
 # Associate the Elastic IP with the Jenkins EC2 instance?
-
 resource "aws_eip_association" "jenkins_eip_assoc" {
 instance_id   = aws_instance.jenkins.id
 allocation_id = aws_eip.jenkins_eip.id
