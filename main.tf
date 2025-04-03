5cd8f41d99e99a81f18f005f6b1499bb765735e0
Miro-DevOps <66559331+merey91@users.noreply.github.com>
2025-04-04 00:02:23 +1100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,32 +1,30 @@
 # Define the Provider
 provider "aws" {
-region = "us-west-2" # All AWS resources created in this Terraform configuration will by default be deployed in the US West (Oregon) region
+region = "ap-southeast-2" # All AWS resources created in this Terraform configuration will by default be deployed in the aus region
 }
 
-# Terraform Backend
+# Configure the backend to use S3
 terraform {
 backend "s3" {
 bucket         = "terraform-state-storage"     # Replace with your actual bucket name
 key            = "jenkins-server/terraform.tfstate" # Path to the state file inside S3
-region         = "us-west-2"
+region         = "ap-southeast-2"
 dynamodb_table = "terraform-lock-table" # Locking mechanism
 encrypt        = true                   # Enable state encryption
 }
 }
 
-# Main resources. Defines an AWS EC2 instance for Jenkins
-resource "aws_instance" "jenkins-server" {  
-ami                    = "ami-" # replace / Amazon Linux 2023 AMI
-instance_type          = "t2.micro"
-key_name               = var.key_name #?
-vpc_security_group_ids = [aws_security_group.jenkins_sg.id]
+# Create an S3 bucket resource
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "my-shuotestbucket"
+  acl    = "private"
 
 user_data = file("${path.module}/user-data.sh") # ? a bootstrapping script that automates the installation of software and system configuration upon instance launch.
 
 tags = {
-Name  = "Jenkins-Server" # aws ec2  instance name
-Usage = "pipeline" #?
-}
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
 }
 
 # Allocate an Elastic IP. Elastic IPs are static IP addresses designed for dynamic cloud computing, providing a fixed IP address for your instance.?
