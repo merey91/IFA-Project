47a731db6ab03bc54d386b6c6ec8f388ae3364fa
Miro-DevOps <66559331+merey91@users.noreply.github.com>
2025-04-04 12:02:23 +1100

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,1 +1,10 @@
-# 定义Terraform状态文件的存储位置，用于追踪所有已部署资源的状态。
+# Configure the backend to use S3
+terraform {
+backend "s3" {
+bucket         = "terraform-state-storage"     # Replace with your actual bucket name
+key            = "jenkins-server/terraform.tfstate" # Path to the state file inside S3
+region         = "ap-southeast-2"
+dynamodb_table = "terraform-lock-table" # Locking mechanism
+encrypt        = true                   # Enable state encryption
+}
+}
