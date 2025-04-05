7261a21c6b0fe6cbf1833edfd080f58ec95b879a
Miro-DevOps <66559331+merey91@users.noreply.github.com>
2025-04-05 20:01:15 +1100

    Create user_data.sh

---
+++ user_data.sh
@@ -0,0 +1,40 @@
+resource "aws_instance" "app_instance" {
+  ami           = "ami-034488765f896f58f"  # Amazon Linux 2 AMI (us-east-1)
+  instance_type = "t2.micro"
+  key_name      = "devops"     # 替换为你的密钥对名称
+
+  vpc_security_group_ids = [aws_security_group.app_sg.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              # 更新系统
+              sudo yum update -y
+              
+              # 安装 Git
+              sudo yum install -y git
+              
+              # 安装 Node.js 20.x（最新LTS版本）
+              curl -fsSL https://rpm.nodesource.com/setup_20.x | sudo bash -
+              sudo yum install -y nodejs
+              
+              # 克隆代码仓库（替换为你的实际仓库地址）
+              git clone https://github.com/Innovate-Future-Association-Translation/translator-app.git /home/ec2-user/app
+              cd /home/ec2-user/app
+              
+              # 安装依赖并构建
+              npm install
+              npm run build
+              
+
+              
+              # 使用PM2持久化运行
+              sudo npm install -g pm2
+              pm2 startup
+              pm2 start npm --name "app" -- run start
+              pm2 save
+              EOF
+
+  tags = {
+    Name = "NodeJS-3000-Port-App"
+  }
+}
