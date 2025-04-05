7c31787937860b26fee211ff13ab42cb7895b265
Miro-DevOps <66559331+merey91@users.noreply.github.com>
2025-04-05 19:26:09 +1100

    Update veriables.tf

--- veriables.tf
+++ veriables.tf
@@ -1,39 +1,19 @@
-# variable for key pair name
-variable "key_name" {
-description = "Name of the AWS SSH Key Pair"
-type        = string
-default     = "ifa-Jenkins-keypair"
-}
-
-# variable for AWS services name
-variable "aws_services" {
-  description = "AWS Services"
-  type        = list(string)
-  default     = ["EC2", "S3", "ECS"]
-}
-
-# variable for region name
 variable "region" {
-  description = "AWS region"
-  default     = "ap-southeast-2"
+  description = "AWS region to deploy resources."
+  default     = "ap-southeast-2"  # AWS region
 }
 
-# variable for bucket name
 variable "bucket_name" {
-  description = "S3 bucket name"
-  default     = "my-unique-bucket-name"
+  description = "Name of the S3 bucket for static website hosting."
+  default     = "ifa-translator-app-bucket"  # Your S3 Bucket Name
 }
 
-# variable for instance name
-variable "instance_name" {
-  description = "Name of the EC2 instance"
-  type        = string
-  default     = "ifa-instance"
+variable "index_document" {
+  description = "The index document of the static website."
+  default     = "index.html"  
 }
 
-# variable for instance type
-variable "instance_type" {
-  description = "Type of EC2 instance"
-  type        = string
-  default     = "t3.micro"
+variable "error_document" {
+  description = "The error document of the static website."
+  default     = "404.html"  
 }
