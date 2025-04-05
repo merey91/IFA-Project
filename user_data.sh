2162460596cdc40fe5a5f2027d4f7b5d943e2430
Miro-DevOps <66559331+merey91@users.noreply.github.com>
2025-04-05 23:51:10 +1100

    Update user_data.sh

--- user_data.sh
+++ user_data.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
-              
+
+# Update your Ubuntu package list.
+sudo apt-get update
+
+# Install the AWS CLI using apt-get.
+sudo apt-get install awscli
+
+# Check the AWS CLI version to confirm that it was installed correctly.
+aws --version
+
+# Configuring AWS CLI
+aws configure
+          
 # Clone the repository url.
 git clone https://github.com/Innovate-Future-Association-Translation/translator-app.git 
 cd translator-app
