e410e31fd2a0fad8fd3cbeb30aea5d18d24a00ea
Miro-DevOps <66559331+merey91@users.noreply.github.com>
2025-04-05 19:54:33 +1100

    Update main.tf

--- main.tf
+++ main.tf
@@ -44,16 +44,16 @@ resource "aws_s3_bucket" "my_bucket" {
 resource "aws_cloudfront_distribution" "s3_distribution" {
   origin {
     domain_name = aws_s3_bucket.my_bucket.website_endpoint # Uses the S3 bucket's website endpoint as the origin for CloudFront. This integrates the S3 static website with CloudFront.
-    origin_id   = "S3-my-translator-app" # A unique identifier for the origin within the CloudFront distribution.
+    origin_id   = "S3-${var.bucket_name}" # A unique identifier for the origin within the CloudFront distribution.
   }
 
   enabled             = true # Enables the CloudFront distribution so it starts serving content.
-  default_root_object = "index.html" # Designates the default document for the root URL, usually the homepage of the website.
+  default_root_object = var.index_document # Designates the default document for the root URL, usually the homepage of the website.
 
   default_cache_behavior {
     allowed_methods  = ["GET", "HEAD", "OPTIONS"] # Specifies which HTTP methods to allow and cache; typical for static content.
     cached_methods   = ["GET", "HEAD"] # Defines methods for which CloudFront caches responses.
-    target_origin_id = "S3-my-translator-app" # Links this cache behavior to the specified origin.
+    target_origin_id = "S3-${var.bucket_name}" # Links this cache behavior to the specified origin.
 
     forwarded_values {
       query_string = false # Specifies not to forward URL query strings to the origin, which can improve caching efficiency for static sites.
@@ -72,8 +72,13 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
       restriction_type = "none" # Indicates no geographic restrictions on who can access the content, typical for global applications.
     }
   }
-
-  viewer_certificate {
-    cloudfront_default_certificate = true # Uses the default SSL/TLS certificate provided by AWS, simplifying HTTPS setup.
-  }
 }
+
+# Deploy static files to S3 Bucket
+resource "aws_s3_bucket_object" "website_files" {
+  bucket = aws_s3_bucket.my_bucket.id  # Specifies the ID of the S3 bucket where files will be stored.
+  key    = "out/"                      # The key path under which the file will be stored in the bucket.
+  source = "path_to_your_local_files/out/"  # The local path to the directory where your output files are stored that you want to upload.
+  etag   = filemd5("path_to_your_local_files/out/index.html")  # The MD5 hash of the local file to manage file integrity.
+}
+
