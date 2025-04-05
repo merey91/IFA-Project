0c12dbb3fc1626c412996fe686f45c3ddcef8ce9
Miro-DevOps <66559331+merey91@users.noreply.github.com>
2025-04-05 20:08:01 +1100

    Update veriables.tf

--- veriables.tf
+++ veriables.tf
@@ -1,8 +1,3 @@
-variable "region" {
-  description = "AWS region to deploy resources."
-  default     = "ap-southeast-2"  # AWS region
-}
-
 variable "bucket_name" {
   description = "Name of the S3 bucket for static website hosting."
   default     = "ifa-translator-app-bucket"  # Your S3 Bucket Name
