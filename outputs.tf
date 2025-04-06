351fc24c6fa44bebc495b44bf65160f549fe453d
Miro-DevOps <66559331+merey91@users.noreply.github.com>
2025-04-06 19:38:02 +1000

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
 output "s3_bucket_name" {
   description = "The name of the bucket used for static website hosting."
-  value       = aws_s3_bucket.my_bucket.bucket
+  value       = aws_s3_bucket.s3_bucket.bucket
 }
 
 output "cloudfront_distribution_id" {
@@ -8,10 +8,11 @@ output "cloudfront_distribution_id" {
   value       = aws_cloudfront_distribution.s3_distribution.id
 }
 
-output "bucket_regional_domain_name" {
-  value = "http://${aws_s3_bucket.my_bucket.bucket_regional_domain_name}"
+output "s3_bucket_website_endpoint" {
+  description = "The website endpoint URL for the S3 bucket"
+  value = aws_s3_bucket.s3_bucket.website_endpoint
 }
 
-output "cloudfront_distribution_domain" {
-  value = "https://${aws_cloudfront_distribution.s3_distribution.domain_name}"
+output "cloudfront_distribution_domain_name" {
+  value = aws_cloudfront_distribution.s3_distribution.domain_name
 }
