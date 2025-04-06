e43ca01bf6aa259cb9f687bacbda0ff4cd332c24
Miro-DevOps <66559331+merey91@users.noreply.github.com>
2025-04-06 13:54:08 +1000

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,12 +12,11 @@ resource "aws_s3_bucket" "my_bucket" {
     index_document = var.index_document  # Specifies the homepage for the static website hosted in this bucket.
     error_document = var.error_document  # Specifies the error page that will be displayed when a requested file is not found.
    }
-
+ }
   resource "aws_s3_bucket_acl" "my_bucket_acl" {
     bucket = aws_s3_bucket.my_bucket.id
     acl    = "public-read"
   }
-}
 
 # Sets a bucket policy to explicitly allow public read access to all objects in the bucket. Necessary for hosting public static websites.
 resource "aws_s3_bucket_policy" "my_bucket_policy" {
@@ -31,7 +30,7 @@ policy = jsonencode({
         Effect = "Allow",
         Principal = "*", # Allows access to "everyone."
         Action = "s3:GetObject",
-        Resource = "arn:aws:s3:::{aws_s3_bucket.my_bucket.bucket}/*"
+        Resource = "${aws_s3_bucket.my_bucket.arn}/*"
       },
     ]
   })
@@ -54,15 +53,14 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
     allowed_methods  = ["GET", "HEAD", "OPTIONS"] # Specifies which HTTP methods to allow and cache; typical for static content.
     cached_methods   = ["GET", "HEAD"] # Defines methods for which CloudFront caches responses.
     target_origin_id = "S3-${var.bucket_name}" # Links this cache behavior to the specified origin.
+    viewer_protocol_policy = "redirect-to-https" 
 
     forwarded_values {
       query_string = false # Specifies not to forward URL query strings to the origin, which can improve caching efficiency for static sites.
       cookies {
         forward = "none" # Indicates that cookies should not be forwarded to the origin, which is typical for static content that doesn't require user sessions.
       }
-    }
-
-    viewer_protocol_policy = "redirect-to-https" # Forces HTTP requests to be redirected to HTTPS, securing the communication.
+    } 
   }
 
   price_class = "PriceClass_All" # Specifies the price class, using all regions to maximize reach and performance.
