f781a112b7165ee0ba39536ebb9eee7302bacb2d
Miro-DevOps <66559331+merey91@users.noreply.github.com>
2025-04-12 22:29:27 +1000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,3 +3,9 @@ variable "domain_name" {
   type        = string
   default     = "miro.aws.jrworkshop.au"
 }
+
+variable "account_id" {
+  description = "AWS account ID"
+  type        = string
+  default     = "027307989935"
+}
