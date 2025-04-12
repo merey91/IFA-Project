86c7d1686c3251d0ee24290d0a13bc1b37f20dd8
Miro-DevOps <66559331+merey91@users.noreply.github.com>
2025-04-12 21:46:39 +1000

    Update frontend-s3.tf

--- frontend-s3.tf
+++ frontend-s3.tf
@@ -1,3 +1,76 @@
+# Specifies the AWS provider for Terraform
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-southeast-2" # Defines the AWS region
+}
+
+# Generates a random 4-byte hexadecimal string to append to the bucket name, ensuring uniqueness, S3 bucket names must be globally unique
+resource "random_id" "bucket_suffix" {
+  byte_length = 4
+}
+
+# Creates an S3 bucket with a unique name for frontend.
+resource "aws_s3_bucket" "frontend_bucket" {
+  bucket = "frontend-site-${random_id.bucket_suffix.hex}"
+
+  website {
+    index_document = "index.html"
+    error_document = "404.html"
+  }
+
+  tags = {
+    Name = "FrontendHosting"
+  }
+}
+
+# Sets object ownership to BucketOwnerPreferred, allowing the bucket owner to control all objects without requiring ACLs.
+resource "aws_s3_bucket_ownership_controls" "ownership" {
+  bucket = aws_s3_bucket.frontend_bucket.id
+
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+# Disables public access restrictions to allow public access to the website files. This is necessary for hosting a public-facing static website.
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket                  = aws_s3_bucket.frontend_bucket.id
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+# Bucket Policy for CloudFront Access
+resource "aws_s3_bucket_policy" "frontend_bucket_policy" {
+  bucket = aws_s3_bucket.frontend_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal: {
+          Service: "cloudfront.amazonaws.com"
+        },
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.frontend_bucket.arn}/*"
+      }
+    ]
+  })
+
+  depends_on = [aws_s3_bucket_public_access_block.public_access]
+}
+
+----
 provider "aws" {
   region              = var.region
   allowed_account_ids = [var.account_id]
