7c03734a86d7780039ddafbe3fdb9410c23265df
Miro-DevOps <66559331+merey91@users.noreply.github.com>
2025-04-12 22:18:00 +1000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,14 +1,5 @@
-variable "bucket_name" {
-  description = "Name of the S3 bucket for static website hosting."
-  default     = "ifa-translator-app-bucket"  # Your S3 Bucket Name
+variable "domain_name" {
+  description = "The root domain name to use for the website (e.g., example.com)"
+  type        = string
 }
 
-variable "index_document" {
-  description = "The index document of the static website."
-  default     = "index.html"
-}
-
-variable "error_document" {
-  description = "The error document of the static website."
-  default     = "404.html"
-}
