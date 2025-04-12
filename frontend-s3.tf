bb07e0871583a47aaff13c1646c1aa2790acfde6
Miro-DevOps <66559331+merey91@users.noreply.github.com>
2025-04-13 09:25:59 +1000

    Update frontend-s3.tf

--- frontend-s3.tf
+++ frontend-s3.tf
@@ -15,14 +15,17 @@ provider "aws" {
 # Creates an S3 bucket for frontend.
 resource "aws_s3_bucket" "frontend_bucket" {
   bucket = replace(var.domain_name, ".", "-")
+}
 
-  website {
-    index_document = "index.html"
-    error_document = "404.html"
+resource "aws_s3_bucket_website_configuration" "frontend_site" {
+  bucket = aws_s3_bucket.frontend_bucket.id
+
+  index_document {
+    suffix = "index.html"
   }
 
-  tags = {
-    Name = "FrontendHosting"
+  error_document {
+    key = "404.html"
   }
 }
 
@@ -42,6 +45,16 @@ resource "aws_s3_bucket_public_access_block" "public_access" {
   block_public_policy     = false
   ignore_public_acls      = false
   restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.ownership,
+    aws_s3_bucket_public_access_block.public_access,
+  ]
+
+  bucket = aws_s3_bucket.example.id
+  acl    = "public-read"
 }
 
 # Bucket Policy for CloudFront Access
