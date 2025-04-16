924c642df97583bcb3652bfa9ef64dae19b9ffd4
Miro-DevOps <66559331+merey91@users.noreply.github.com>
2025-04-16 23:28:41 +1000

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,3 @@
-# Outputting Website URL
-output "website_url" {
-  description = "The website URL for the S3 bucket"
-  value = "https://www.${var.domain_name}" 
-}
-
 # 输出静态网站的 URL
 output "website_endpoint" {
   value = aws_s3_bucket.frontend_bucket.website_endpoint
