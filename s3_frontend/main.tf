d3004e832918d05c1314f59bd3a525c2496e0a1e
Miro-DevOps <66559331+merey91@users.noreply.github.com>
2025-04-19 10:13:15 +1000

    Update main.tf

--- s3_frontend/main.tf
+++ s3_frontend/main.tf
@@ -1,1 +1,193 @@
+# Specifies the AWS provider for Terraform
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
 
+provider "aws" {
+  region = "ap-southeast-2" Defines the AWS region
+}
+
+# Create a new S3 bucket
+resource "aws_s3_bucket" "frontend_bucket" {
+  bucket = var.bucket_name
+
+  website {
+    index_document = "index.html"
+    error_document = "404.html"
+  }
+
+  tags = {
+    Name = "FrontendHosting"
+  }
+}
+
+# Set Object Ownership Control
+resource "aws_s3_bucket_ownership_controls" "ownership" {
+  bucket = aws_s3_bucket.frontend_bucket.id
+
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+# Set up public access settings
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket                  = aws_s3_bucket.frontend_bucket.id
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+# Set the S3 bucket policy
+resource "aws_s3_bucket_policy" "public_read" {
+  bucket = aws_s3_bucket.frontend_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.frontend_bucket.arn}/*"
+      }
+    ]
+  })
+
+  depends_on = [aws_s3_bucket_public_access_block.public_access]
+}
+
+# Upload static website files
+resource "aws_s3_bucket_object" "frontend_files" {
+  for_each = fileset("out", "**")
+
+  bucket = aws_s3_bucket.frontend_bucket.id
+  key    = each.value
+  source = "out/${each.value}"
+  etag   = filemd5("out/${each.value}")
+
+  content_type = lookup(
+    {
+      html = "text/html"
+      css  = "text/css"
+      js   = "application/javascript"
+      json = "application/json"
+      png  = "image/png"
+      jpg  = "image/jpeg"
+      jpeg = "image/jpeg"
+      svg  = "image/svg+xml"
+      ico  = "image/x-icon"
+      txt  = "text/plain"
+    },
+    regex("[^.]+$", each.value),
+    "application/octet-stream"
+  )
+
+  depends_on = [
+    aws_s3_bucket_policy.public_read
+  ]
+}
+
+# Request ACM certificate
+resource "aws_acm_certificate" "certificate" {
+  domain_name       = "${var.subdomain}.${var.domain_name}" 
+  validation_method = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+# Use an existing Route 53 hosted zone
+data "aws_route53_zone" "existing_zone" {
+  name = var.domain_name
+}
+
+# validate ACM certificate
+resource "aws_route53_record" "certificate_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.certificate.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  zone_id = data.aws_route53_zone.existing_zone.zone_id
+  name    = each.value.name
+  type    = each.value.type
+  records = [each.value.record]
+  ttl     = 60
+}
+
+# Waiting for the ACM certificate to be validated
+resource "aws_acm_certificate_validation" "validation" {
+  certificate_arn         = aws_acm_certificate.certificate.arn
+  validation_record_fqdns = [for record in aws_route53_record.certificate_validation : record.fqdn]
+}
+
+# Create a CloudFront distribution
+resource "aws_cloudfront_distribution" "frontend_distribution" {
+  origin {
+    domain_name = aws_s3_bucket.frontend_bucket.website_endpoint
+    origin_id   = "S3-${aws_s3_bucket.frontend_bucket.bucket}"
+
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1.2"]
+    }
+  }
+
+  enabled             = true
+  default_root_object = "index.html"
+
+  aliases = ["${var.subdomain}.${var.domain_name}"] 
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "S3-${aws_s3_bucket.frontend_bucket.bucket}"
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    acm_certificate_arn = aws_acm_certificate.certificate.arn
+    ssl_support_method  = "sni-only"
+  }
+}
+
+# Configure Route 53 DNS records to point to CloudFront.
+resource "aws_route53_record" "alias_to_cloudfront" {
+  zone_id = data.aws_route53_zone.existing_zone.zone_id
+  name    = "${var.subdomain}.${var.domain_name}"
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.frontend_distribution.domain_name
+    zone_id                = aws_cloudfront_distribution.frontend_distribution.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
