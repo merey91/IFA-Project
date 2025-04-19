090cf4522a7661a55cf9a9f8af91c49f25ee7d19
Miro-DevOps <66559331+merey91@users.noreply.github.com>
2025-04-19 10:16:00 +1000

    Update variables.tf

--- s3_frontend/variables.tf
+++ s3_frontend/variables.tf
@@ -1,1 +1,15 @@
+variable "bucket_name" {
+  description = "The unique name of the S3 bucket"
+  type        = string
+}
 
+variable "domain_name" {
+  description = "Your domain name (e.g., example.com)"
+  type        = string
+  default     = "miro.aws.jrworkshop.au"
+}
+
+variable "subdomain" {
+  description = "Your subdomain (e.g., www)"
+  type        = string
+}
