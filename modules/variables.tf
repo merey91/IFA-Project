9599ff8bf94baf327e92e78f3f74d851b4831131
Miro-DevOps <66559331+merey91@users.noreply.github.com>
2025-05-16 01:03:52 +1000

    Delete modules directory

--- modules/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "bucket_name" {
-  description = "The name of the S3 bucket"
-  type        = string
-}
-
-variable "parameter_name" {
-  description = "The name of the SSM parameter"
-  type        = string
-}
